79c147f20b9643069902d032e0c75c972db5a30e
joshafouda <josh.aremou229@gmail.com>
2024-10-13 17:09:54 +0200

    build packaage and upload to pypi successfully

---
+++ example_script.py
@@ -0,0 +1,7 @@
+import impyrialafouda
+
+result_length = impyrialafouda.length.convert_unit(10, 'in', 'yd')
+print(result_length)
+
+result_weight = impyrialafouda.weight.convert_unit(2, 'lb', 'oz')
+print(result_weight)
